7697072e2b9877fc71800884fcccb27b9beb4872
Christian Hoffmeister <mail@choffmeister.de>
2013-11-14 20:48:59 +0100

    Create BinaryReader and BinaryWriter

---
+++ core/src/main/scala/de/choffmeister/secloud/core/utils/BinaryReaderWriter.scala
@@ -0,0 +1,84 @@
+package de.choffmeister.secloud.core.utils
+
+import java.io.OutputStream
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class BinaryWriter(val stream: OutputStream) {
+  private val bufRaw = new Array[Byte](8)
+  private val buf = ByteBuffer.wrap(bufRaw)
+  buf.order(ByteOrder.BIG_ENDIAN)
+
+  def writeInt8(value: Byte): Unit = {
+    buf.put(0, value)
+    stream.write(bufRaw, 0, 1)
+  }
+
+  def writeInt16(value: Short): Unit = {
+    buf.putShort(0, value)
+    stream.write(bufRaw, 0, 2)
+  }
+
+  def writeInt32(value: Int): Unit = {
+    buf.putInt(0, value)
+    stream.write(bufRaw, 0, 4)
+  }
+
+  def writeInt64(value: Long): Unit = {
+    buf.putLong(0, value)
+    stream.write(bufRaw, 0, 8)
+  }
+
+  def writeBoolean(value: Boolean): Unit = value match {
+    case true => stream.write(Array(1.toByte))
+    case false => stream.write(Array(0.toByte))
+  }
+
+  def writeString(value: String): Unit = {
+    val bytes = value.getBytes("UTF-8")
+    writeInt32(bytes.length)
+    stream.write(bytes)
+  }
+
+  def close(): Unit = stream.close()
+}
+
+class BinaryReader(val stream: InputStream) {
+  private val bufRaw = new Array[Byte](8)
+  private val buf = ByteBuffer.wrap(bufRaw)
+  buf.order(ByteOrder.BIG_ENDIAN)
+
+  def readInt8(): Byte = {
+    stream.read(bufRaw, 0, 1)
+    return buf.get(0)
+  }
+
+  def readInt16(): Short = {
+    stream.read(bufRaw, 0, 2)
+    return buf.getShort(0)
+  }
+
+  def readInt32(): Int = {
+    stream.read(bufRaw, 0, 4)
+    return buf.getInt(0)
+  }
+
+  def readInt64(): Long = {
+    stream.read(bufRaw, 0, 8)
+    return buf.getLong(0)
+  }
+
+  def readBoolean(): Boolean = {
+    return readInt8() != 0
+  }
+
+  def readString(): String = {
+    val length = readInt32()
+    val stringBuf = new Array[Byte](length)
+    stream.read(stringBuf, 0, length)
+    return new String(stringBuf, "UTF-8")
+  }
+
+  def close(): Unit = stream.close()
+}
---
+++ core/src/test/scala/de/choffmeister/secloud/core/utils/BinaryReaderWriterSpec.scala
@@ -0,0 +1,116 @@
+package de.choffmeister.secloud.core.utils
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+@RunWith(classOf[JUnitRunner])
+class BinaryReaderWriterSpec extends Specification {
+  "BinaryReaderWriter" should {
+    "read and write Int8" in {
+      val streamWrite = new ByteArrayOutputStream(128)
+      val writer = new BinaryWriter(streamWrite)
+
+      writer.writeInt8(Byte.MinValue)
+      writer.writeInt8(Byte.MaxValue)
+      writer.close()
+
+      val buf = streamWrite.toByteArray()
+
+      val streamRead = new ByteArrayInputStream(buf)
+      val reader = new BinaryReader(streamRead)
+
+      reader.readInt8() === Byte.MinValue
+      reader.readInt8() === Byte.MaxValue
+    }
+
+    "read and write Int16" in {
+      val streamWrite = new ByteArrayOutputStream(128)
+      val writer = new BinaryWriter(streamWrite)
+
+      writer.writeInt16(Short.MinValue)
+      writer.writeInt16(Short.MaxValue)
+      writer.close()
+
+      val buf = streamWrite.toByteArray()
+
+      val streamRead = new ByteArrayInputStream(buf)
+      val reader = new BinaryReader(streamRead)
+
+      reader.readInt16() === Short.MinValue
+      reader.readInt16() === Short.MaxValue
+    }
+
+    "read and write Int32" in {
+      val streamWrite = new ByteArrayOutputStream(128)
+      val writer = new BinaryWriter(streamWrite)
+
+      writer.writeInt32(Int.MinValue)
+      writer.writeInt32(Int.MaxValue)
+      writer.close()
+
+      val buf = streamWrite.toByteArray()
+
+      val streamRead = new ByteArrayInputStream(buf)
+      val reader = new BinaryReader(streamRead)
+
+      reader.readInt32() === Int.MinValue
+      reader.readInt32() === Int.MaxValue
+    }
+
+    "read and write Int64" in {
+      val streamWrite = new ByteArrayOutputStream(128)
+      val writer = new BinaryWriter(streamWrite)
+
+      writer.writeInt64(Long.MinValue)
+      writer.writeInt64(Long.MaxValue)
+      writer.close()
+
+      val buf = streamWrite.toByteArray()
+
+      val streamRead = new ByteArrayInputStream(buf)
+      val reader = new BinaryReader(streamRead)
+
+      reader.readInt64() === Long.MinValue
+      reader.readInt64() === Long.MaxValue
+    }
+
+    "read and write Boolean" in {
+      val streamWrite = new ByteArrayOutputStream(128)
+      val writer = new BinaryWriter(streamWrite)
+
+      writer.writeBoolean(true)
+      writer.writeBoolean(false)
+      writer.close()
+
+      val buf = streamWrite.toByteArray()
+
+      val streamRead = new ByteArrayInputStream(buf)
+      val reader = new BinaryReader(streamRead)
+
+      reader.readBoolean() === true
+      reader.readBoolean() === false
+    }
+
+    "read and write String" in {
+      val streamWrite = new ByteArrayOutputStream(128)
+      val writer = new BinaryWriter(streamWrite)
+
+      writer.writeString("foo")
+      writer.writeString("")
+      writer.writeString("äöü")
+      writer.close()
+
+      val buf = streamWrite.toByteArray()
+
+      val streamRead = new ByteArrayInputStream(buf)
+      val reader = new BinaryReader(streamRead)
+
+      reader.readString() === "foo"
+      reader.readString() === ""
+      reader.readString() === "äöü"
+    }
+  }
+}
