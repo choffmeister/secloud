a1fec41cd01d54a3111b9ec74526e024444d0aa6
Christian Hoffmeister <mail@choffmeister.de>
2013-11-16 16:47:52 +0100

    Implement Keystore

---
+++ core/src/test/scala/de/choffmeister/secloud/core/security/KeyStoreSpec.scala
@@ -0,0 +1,48 @@
+package de.choffmeister.secloud.core.security
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.netbeans.api.keyring.Keyring
+
+@RunWith(classOf[JUnitRunner])
+class KeyStoreSpec extends Specification {
+  "KeyStrore" should {
+    "save and load private keys" in {
+      val randomString = randomKeyName
+
+      KeyStore.savePrivateKey(randomString, "vErYsEcReTpAsSwOrD")
+      KeyStore.loadPrivateKey(randomString) === Some("vErYsEcReTpAsSwOrD")
+      KeyStore.removePrivateKey(randomString)
+      ok
+    }
+
+    "return none if private key does not exist" in {
+      val randomString = randomKeyName
+
+      KeyStore.loadPrivateKey(randomString) must beNone
+    }
+
+    "remove private keys" in {
+      val randomString = randomKeyName
+
+      KeyStore.savePrivateKey(randomString, "vErYsEcReTpAsSwOrD")
+      KeyStore.loadPrivateKey(randomString) must beSome
+      KeyStore.removePrivateKey(randomString)
+      KeyStore.loadPrivateKey(randomString) must beNone
+    }
+
+    "override existing private keys" in {
+      val randomString = randomKeyName
+
+      KeyStore.savePrivateKey(randomString, "vErYsEcReTpAsSwOrD")
+      KeyStore.loadPrivateKey(randomString) === Some("vErYsEcReTpAsSwOrD")
+      KeyStore.savePrivateKey(randomString, "vErYsEcReTpAsSwOrD-2")
+      KeyStore.loadPrivateKey(randomString) === Some("vErYsEcReTpAsSwOrD-2")
+      KeyStore.removePrivateKey(randomString)
+      ok
+    }
+  }
+
+  def randomKeyName = java.util.UUID.randomUUID().toString()
+}
