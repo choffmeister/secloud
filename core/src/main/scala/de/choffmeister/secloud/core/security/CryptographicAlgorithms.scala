7039194ef62f553593bc2a0baf1a953e2a3d326b
Christian Hoffmeister <mail@choffmeister.de>
2013-11-17 20:01:44 +0100

    Enrich SymmetricEncryptionAlgorithm to provide methods to read/write the key and wrap streams

--- core/src/main/scala/de/choffmeister/secloud/core/security/CryptographicAlgorithms.scala
+++ core/src/main/scala/de/choffmeister/secloud/core/security/CryptographicAlgorithms.scala
@@ -1,20 +1,75 @@
 package de.choffmeister.secloud.core.security
 
+import java.io.InputStream
+import java.io.OutputStream
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.Cipher
+import javax.crypto.CipherInputStream
+import javax.crypto.CipherOutputStream
+import javax.crypto.spec.SecretKeySpec
+import de.choffmeister.secloud.core.utils.BinaryReaderWriter._
+import java.security.SecureRandom
+import java.security.spec.AlgorithmParameterSpec
+import javax.crypto.spec.IvParameterSpec
+
 object CryptographicAlgorithms {
+  case class SymmetricEncryptionParameters(algorithm: SymmetricEncryptionAlgorithm, key: SecretKey, iv: IvParameterSpec)
+
   sealed abstract class SymmetricEncryptionAlgorithm {
     val algorithmName: String
     val blockModeName: String
     val paddingName: String
-    val fullAlgorithmName: String = s"$algorithmName/$blockModeName/$paddingName"
+    def fullAlgorithmName: String = s"$algorithmName/$blockModeName/$paddingName"
 
     val blockSize: Int
     val keySize: Int
+    def blockSizeBits: Int = blockSize * 8
+    def keySizeBits: Int = keySize * 8
+    
+    def generateKey(): SymmetricEncryptionParameters = {
+      val random = new SecureRandom()
+      val keyGen = KeyGenerator.getInstance(algorithmName)
+      keyGen.init(keySizeBits, random)
+      val key = keyGen.generateKey()
+      val ivBinary = new Array[Byte](blockSize)
+      random.nextBytes(ivBinary)
+      val params = new IvParameterSpec(ivBinary)
+      
+      SymmetricEncryptionParameters(this, key, params)
+    }
+
+    def readParameters(stream: InputStream): SymmetricEncryptionParameters = {
+      val keyBinary = stream.readBinary()
+      val key = new SecretKeySpec(keyBinary, 0, keyBinary.length, algorithmName)
+      val ivBinary = stream.readBinary()
+      val params = new IvParameterSpec(ivBinary)
+
+      SymmetricEncryptionParameters(this, key, params)
+    }
+
+    def writeParameters(stream: OutputStream, parameters: SymmetricEncryptionParameters) {
+      stream.writeBinary(parameters.key.getEncoded)
+      stream.writeBinary(parameters.iv.getIV)
+    }
+
+    def wrapStream(stream: InputStream, parameters: SymmetricEncryptionParameters): CipherInputStream = {
+      val cipher = Cipher.getInstance(fullAlgorithmName);
+      cipher.init(Cipher.DECRYPT_MODE, parameters.key, parameters.iv);
+      new CipherInputStream(stream, cipher);
+    }
+
+    def wrapStream(stream: OutputStream, parameters: SymmetricEncryptionParameters): CipherOutputStream = {
+      val cipher = Cipher.getInstance(fullAlgorithmName);
+      cipher.init(Cipher.ENCRYPT_MODE, parameters.key, parameters.iv);
+      new CipherOutputStream(stream, cipher);
+    }
   }
 
-  sealed abstract class AES {
+  sealed abstract class AES extends SymmetricEncryptionAlgorithm {
     val algorithmName = "AES"
     val blockModeName = "CBC"
-    val paddingName = "NoPadding"
+    val paddingName = "PKCS5Padding"
 
     val blockSize = 16
   }
---
+++ core/src/test/scala/de/choffmeister/secloud/core/security/CryptographicAlgorithmsSpec.scala
@@ -0,0 +1,58 @@
+package de.choffmeister.secloud.core.security
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import de.choffmeister.secloud.core.utils.BinaryReaderWriter._
+import de.choffmeister.secloud.core.security.CryptographicAlgorithms._
+import java.io.ByteArrayOutputStream
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import javax.crypto.SecretKey
+import org.apache.commons.codec.binary.Hex
+
+@RunWith(classOf[JUnitRunner])
+class CryptographicAlgorithmsSpec extends Specification {
+  "SymmetricEncryptionAlgorithm" should {
+    "wrap streams with AES-128" in {
+      testForConcreteAlgorithm(`AES-128`, "") === ""
+      testForConcreteAlgorithm(`AES-128`, "Hello") === "Hello"
+      testForConcreteAlgorithm(`AES-128`, "Hello World!") === "Hello World!"
+    }
+
+    "wrap streams with AES-192" in {
+      testForConcreteAlgorithm(`AES-192`, "") === ""
+      testForConcreteAlgorithm(`AES-192`, "Hello") === "Hello"
+      testForConcreteAlgorithm(`AES-192`, "Hello World!") === "Hello World!"
+    }
+
+    "wrap streams with AES-256" in {
+      testForConcreteAlgorithm(`AES-256`, "") === ""
+      testForConcreteAlgorithm(`AES-256`, "Hello") === "Hello"
+      testForConcreteAlgorithm(`AES-256`, "Hello World!") === "Hello World!"
+    }
+  }
+
+  def testForConcreteAlgorithm(algorithm: SymmetricEncryptionAlgorithm, text: String): String = {
+    val key1 = algorithm.generateKey()
+    val ms1 = new ByteArrayOutputStream()
+    val cs1 = algorithm.wrapStream(ms1, key1)
+    cs1.writeString(text)
+    cs1.close()
+
+    val binary = ms1.toByteArray
+
+    val key2 = serializeAndDeserializeParameters(algorithm, key1)
+    val ms2 = new ByteArrayInputStream(binary)
+    val cs2 = algorithm.wrapStream(ms2, key2)
+
+    cs2.readString()
+  }
+  
+  def serializeAndDeserializeParameters(algorithm: SymmetricEncryptionAlgorithm, key: SymmetricEncryptionParameters): SymmetricEncryptionParameters = {
+    val out = new ByteArrayOutputStream()
+    algorithm.writeParameters(out, key)
+    val in = new ByteArrayInputStream(out.toByteArray)
+    algorithm.readParameters(in)
+  }
+}
