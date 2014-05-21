4d5ade9825790296d50bf7eab298857888f7a43a
Christian Hoffmeister <hoffmeister@mailbox.org>
2014-05-22 00:29:46 +0200

    Version 0.0.2

--- core/src/main/scala/net/secloud/core/Secloud.scala
+++ core/src/main/scala/net/secloud/core/Secloud.scala
@@ -2,7 +2,7 @@ package net.secloud.core
 
 object Secloud {
   val name = "secloud"
-  val version = "0.0.1"
+  val version = "0.0.2"
   val year = 2014
   val copyright = "Christian Hoffmeister"
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.SbtScalariform._
 object Build extends sbt.Build {
   lazy val commonSettings = Defaults.defaultSettings ++ Seq(
     organization := "net.secloud",
-    version := "0.0.1",
+    version := "0.0.2",
     scalaVersion := "2.10.4",
     scalacOptions ++= Seq("-encoding", "utf8"),
     scalacOptions <<= baseDirectory.map(bd => Seq("-sourcepath", bd.getAbsolutePath)),
